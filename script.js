0abd98c7a6d9efec71429b5d4cfb049c361f8733
joaovnpt <joaovitorguiotti8@gmail.com>
2023-11-29 15:20:43 -0300

    feito menu novo e mobile

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.toggle("sidebar-aberta");
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("sidebar-aberta");
 }
 
 
-  var linksSidebar = document.querySelectorAll(".sidebar");
-  
-  for (var i = 0; i < linksSidebar.length; i++) {
-    linksSidebar[i].addEventListener("click", toggleSidebar);
-  }
+var linksSidebar = document.querySelectorAll(".sidebar");
 
-  function closeSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    sidebar.classList.remove("sidebar-open");
-  }
-  
-  
+for (var i = 0; i < linksSidebar.length; i++) {
+  linksSidebar[i].addEventListener("click", toggleSidebar);
+}
+
+function closeSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("sidebar-open");
+}
